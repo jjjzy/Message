e7bccd48a14829f5d51412cd3b32f1b3fb96023d
jjjzy <yujiazhenn@hotmail.com>
2022-04-16 23:24:11 -0500

    adding sql files

---
+++ messaging sql.sql
@@ -0,0 +1,26 @@
+create database messaging_test
+use messaging
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `nickname` varchar(128) DEFAULT NULL,
+  `gender` varchar(128) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `register_time` datetime DEFAULT NULL,
+  `login_token` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  `last_login_time` datetime DEFAULT NULL,
+  `is_valid` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `user_validation_code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `validation_code` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
