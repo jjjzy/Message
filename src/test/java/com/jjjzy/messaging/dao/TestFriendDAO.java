a9b0926e1ca7c980ef66e4aa9d599c70fc38225a
jjjzy <yujiazhenn@hotmail.com>
2022-04-17 17:43:59 -0500

    friend integration test not finished

---
+++ src/test/java/com/jjjzy/messaging/dao/TestFriendDAO.java
@@ -0,0 +1,29 @@
+package com.jjjzy.messaging.dao;
+
+import com.jjjzy.messaging.Enums.FriendInvitationStatus;
+import com.jjjzy.messaging.Models.FriendInvitation;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface TestFriendDAO {
+    @Select("SELECT * from friend_invitation where (from_user_id=#{userId} or to_user_id=#{userId}) and status=#{status}")
+    List<FriendInvitation> getInvitationByIdAndStatus(@Param("userId") int userId,
+                                                      @Param("status") FriendInvitationStatus status);
+
+    @Insert("INSERT INTO `friend_invitation` (from_user_id, to_user_id, send_time, message, status) VALUES (#{fromUserId}, #{toUserId}, #{sendTime}, #{message}, #{status})")
+    int insertInvitation(FriendInvitation friendInvitation);
+
+    @Update("UPDATE `friend_invitation` SET status = #{status} WHERE id = #{invitationId}")
+    void updateInvitationStatus(@Param("invitationId") int invitationId,
+                                @Param("status") FriendInvitationStatus status);
+
+    @Select("SELECT * from friend_invitation where id = #{invitationId}")
+    FriendInvitation getInvitationById(@Param("invitationId") int invitationId);
+
+    @Delete("truncate table friend_invitation;")
+    void deleteAllInvitations();
+}
--- src/test/java/com/jjjzy/messaging/integrationTest/FriendControllerIntegrationTest.java
+++ src/test/java/com/jjjzy/messaging/integrationTest/FriendControllerIntegrationTest.java
@@ -1,15 +1,186 @@
 package com.jjjzy.messaging.integrationTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jjjzy.messaging.Enums.FriendInvitationStatus;
+import com.jjjzy.messaging.Models.FriendInvitation;
+import com.jjjzy.messaging.Models.User;
+import com.jjjzy.messaging.dao.TestFriendDAO;
+import com.jjjzy.messaging.dao.TestUserDAO;
+import com.jjjzy.messaging.dao.TestUserValidationCodeDAO;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FriendControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private TestUserDAO testUserDAO;
+
+    @Autowired
+    private TestFriendDAO testFriendDAO;
+
+    @Autowired
+    private TestUserValidationCodeDAO testUserValidationCodeDAO;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @BeforeEach
+//    @Order(0)
+//    public void cleanUp1() throws Exception {
+//        testUserDAO.deleteAllUsers();
+//        testUserDAO.deleteAllUserValidationCode();
+//    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        testUserDAO.deleteAllUsers();
+        testUserDAO.deleteAllUserValidationCode();
+        testFriendDAO.deleteAllInvitations();
+
+        String requestBody = "{\"username\": \"george1\", \"password\": \"123\", \"repeatPassword\": \"123\", \"email\": \"nouseage999@gmail.com\"}";
+        this.mockMvc.perform(post("/users/register")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+        String validationCode = this.testUserValidationCodeDAO.findUserValidationCodeByUserId(this.testUserDAO.findUserByUsername("george1").getId()).getValidationCode();
+        this.mockMvc.perform(get("/users/activate")
+                        .contentType("application/json")
+                        .param("username", "george1")
+                        .param("validationCode", validationCode))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+        this.mockMvc.perform(post("/users/login")
+                        .contentType("application/json")
+                        .param("username", "george1")
+                        .param("password", "123"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+
+
+        requestBody = "{\"username\": \"george2\", \"password\": \"123\", \"repeatPassword\": \"123\", \"email\": \"nouseage999@gmail.com\"}";
+        this.mockMvc.perform(post("/users/register")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+        validationCode = this.testUserValidationCodeDAO.findUserValidationCodeByUserId(this.testUserDAO.findUserByUsername("george2").getId()).getValidationCode();
+        this.mockMvc.perform(get("/users/activate")
+                        .contentType("application/json")
+                        .param("username", "george2")
+                        .param("validationCode", validationCode))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+        this.mockMvc.perform(post("/users/login")
+                        .contentType("application/json")
+                        .param("username", "george2")
+                        .param("password", "123"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+    }
+
+    @Test
+    public void testInviteFriend_happyCase() throws Exception {
+        User user1 = this.testUserDAO.findUserByUsername("george1");
+        User user2 = this.testUserDAO.findUserByUsername("george2");
+
+
+        String loginToken = user1.getLoginToken();
+        this.mockMvc.perform(post("/friends/send")
+                        .contentType("application/json")
+                        .header("loginToken", loginToken)
+                        .param("toUserId", Integer.toString(user2.getId()))
+                        .param("message", "hey, this is a test"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+
+        FriendInvitation friendInvitation = this.testFriendDAO.getInvitationByIdAndStatus(user1.getId(), FriendInvitationStatus.PENDING).get(0);
+        assertEquals(friendInvitation.getFromUserId(), user1.getId());
+        assertEquals(friendInvitation.getToUserId(), user2.getId());
+        assertEquals(friendInvitation.getMessage(), "hey, this is a test");
+        assertEquals(friendInvitation.getId(), 1);
+    }
+
+    @Test
+    public void testGetPendingFriends_happyCase() throws Exception {
+        User user1 = this.testUserDAO.findUserByUsername("george1");
+        User user2 = this.testUserDAO.findUserByUsername("george2");
+
+        String loginToken = user1.getLoginToken();
+        this.mockMvc.perform(post("/friends/send")
+                        .contentType("application/json")
+                        .header("loginToken", loginToken)
+                        .param("toUserId", Integer.toString(user2.getId()))
+                        .param("message", "hey, this is a test"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")));
+
+        MvcResult result = this.mockMvc.perform(get("/friends/getPending")
+                        .contentType("application/json")
+                        .header("loginToken", loginToken))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", "application/json"))
+                .andExpect(jsonPath("$.code", is(1000)))
+                .andExpect(jsonPath("$.message", is("Successful")))
+                .andExpect(jsonPath("$.invitations[0].fromUserId", is(1)))
+                .andExpect(jsonPath("$.invitations[0].toUserId", is(2)))
+                .andExpect(jsonPath("$.invitations[0].message", is("hey, this is a test")))
+                .andExpect(jsonPath("$.invitations[0].status", is("PENDING")))
+                .andReturn();
+
+
+        JSONObject json = new JSONObject(result.getResponse().getContentAsString());
+        System.out.println(json.getString("invitations"));
+//        System.out.println(json.getString(""));
+//        assertEquals(array.getJSONObject(2).getString("fromUserId"), 1);
+//        assertEquals(array.getJSONObject(2).getString("toUserId"), 2);
+//        assertEquals(array.getJSONObject(2).getString("status"), "PENDING");
+//        assertEquals(array.getJSONObject(2).getString("message"), "hey, this is a test");
+    }
 }
